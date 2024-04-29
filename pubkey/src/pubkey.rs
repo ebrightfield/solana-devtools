76e496da420b3069285321903969ec1186021549
eric <>
2024-04-29 09:24:42 -0600

    cleanup, tests

--- pubkey/src/pubkey.rs
+++ pubkey/src/pubkey.rs
@@ -1,10 +1,10 @@
-use std::str::FromStr;
 use solana_sdk::pubkey::{ParsePubkeyError, Pubkey};
+use std::str::FromStr;
 
 use thiserror::Error;
 
 const TARGET_KEY_LENGTH: usize = 44;
-const MIN_IDENTIFIER_LENGTH: usize = 12;
+const MIN_PAD_LENGTH: usize = 12;
 const NAMED_ADDRESS_PAD_CHAR: char = '2';
 
 #[derive(Error, Debug, Clone, PartialEq, Eq)]
@@ -17,11 +17,9 @@ pub enum NamedPubkeyError {
     InvalidIdentifier,
 }
 
-pub fn get_named_pubkey(
-    identifier: String
-) -> Result<Pubkey, NamedPubkeyError> {
+pub fn get_named_pubkey(identifier: String) -> Result<Pubkey, NamedPubkeyError> {
     let pad_length = TARGET_KEY_LENGTH - identifier.len();
-    if pad_length < MIN_IDENTIFIER_LENGTH {
+    if pad_length < MIN_PAD_LENGTH {
         return Err(NamedPubkeyError::InvalidIdentifier);
     }
 
@@ -36,12 +34,12 @@ pub fn get_named_pubkey(
         result.push(NAMED_ADDRESS_PAD_CHAR);
     }
 
-    for pad_trim in 0..=MIN_IDENTIFIER_LENGTH {
+    for pad_trim in 0..=MIN_PAD_LENGTH {
         let new_result = &result[..(TARGET_KEY_LENGTH - pad_trim)];
         match Pubkey::from_str(new_result) {
             Ok(key) => return Ok(key),
             Err(ParsePubkeyError::WrongSize) => continue,
-            Err(_) => return Err(NamedPubkeyError::PubkeyInvalidEncoding)
+            Err(_) => return Err(NamedPubkeyError::PubkeyInvalidEncoding),
         }
     }
     Err(NamedPubkeyError::PubkeyWrongSize)
@@ -49,21 +47,28 @@ pub fn get_named_pubkey(
 
 #[cfg(test)]
 mod tests {
-    use rand::{Rng, thread_rng};
-    use rand::distributions::Alphanumeric;
     use super::*;
+    use rand::distributions::Alphanumeric;
+    use rand::{thread_rng, Rng};
 
     #[test]
     fn check_base58_pubkey_validity() {
         for _ in 0..1000 {
             let identifier: String = thread_rng()
                 .sample_iter(&Alphanumeric)
-                .take(TARGET_KEY_LENGTH - MIN_IDENTIFIER_LENGTH)
+                .take(TARGET_KEY_LENGTH - MIN_PAD_LENGTH)
                 .map(|x| x as char)
                 .collect();
 
             get_named_pubkey(identifier).expect("Invalid base58 pubkey");
         }
     }
-}
 
+    #[test]
+    fn various_names() {
+        let _ = get_named_pubkey("myname".to_string()).unwrap();
+        let _ = get_named_pubkey("my_name".to_string()).unwrap_err();
+        let _ = get_named_pubkey("myname123".to_string()).unwrap();
+        let _ = get_named_pubkey("myreallylongname123456789iiiiiii".to_string()).unwrap();
+    }
+}
